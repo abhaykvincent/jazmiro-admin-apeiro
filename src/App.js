d85ed87ef3835a1e3cc420fdd576d0b9a4815443
Abhay <abhaykvincent@gmail.com>
2022-01-28 21:59:27 -0500

    Store and Navigation MIGRATED

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {useSelector, useDispatch} from 'react-redux';
 import Product from './pages/products/Product';
 import Navigation from './components/navigation.jsx'
 import {addPageHistory,changePage,openSheet,closeSheet} from './features/history';
+import Store from './pages/store/Store';
 import Home from './pages/home/Home';;
 
 
@@ -28,7 +29,7 @@ function App() {
   const [orders, setOrders] = useState([1,2,3,4,5,6]);
 
   
-  //close spesipc sheet
+  //close spesipc sheet   
   
 
   return (
@@ -804,60 +805,7 @@ function App() {
         </div>
       
       {/* Store - PAGE*/}
-      <div className="page store hide  ">
-        <div className="page-inner">
-
-          <div className="page-header">
-            <div className="page-header-inner">
-              <div className="header-button"
-                onClick={
-                  () => {
-                    dispatch(changePage('home'));
-                  }
-                }
-              >
-                <div className="icon"></div>
-                <div className="label">Home</div>
-              </div>
-              <div className="header-title">
-                <h3>Store</h3>
-              </div>
-              <div className="option">:</div>
-            </div>
-          </div>
-          <div className="page-content">
-            <div className="page-content-inner">
-            <section className="quick-menus">
-
-              <div className="quick-menu">
-                <div className="menu-icon"></div>
-                <div className="menu-title">Customers</div>
-                <div className="menu-action"></div>
-              </div>
-              <div className="quick-menu">
-                <div className="menu-icon"></div>
-                <div className="menu-title">Discounts</div>
-                <div className="menu-action"></div>
-              </div>
-              <div className="quick-menu">
-                <div className="menu-icon"></div>
-                <div className="menu-title">Settings</div>
-                <div className="menu-action"></div>
-              </div>
-              <div className="quick-menu">
-                <div className="menu-icon"></div>
-                <div className="menu-title">Support</div>
-                <div className="menu-action"></div>
-              </div>
-
-              </section>
-
-            </div>
-          </div>
-
-        </div>
-      </div>
-      
+      <Store/>
       <div className="sheet-cover-black"
         onClick={
           () => {
